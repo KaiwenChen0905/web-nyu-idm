8ec94737d1e1e86b6fc37e8171d32e74a78bee71
KaiwenChen0905 <kevinchen0905@hotmail.com>
2020-03-12 15:58:39 -0400

    first commit

--- javascript-playground/demo.js
+++ javascript-playground/demo.js
@@ -3,3 +3,22 @@
 // watch later: https://www.youtube.com/watch?v=XgSjoHgy3Rk
 
 let namesArray = ['Kevin','McCoy','Emily','Frederic','Alisha','Ashli','Simon','Ty','Fiona','Prismo','Sammy','Caroline','Francesca','Karl','Esther','Irene','Martin','Yanxin','Katrina','Kiana'];
+
+function getRandom(max){
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+let n = getRandom(namesArray.length)
+let randomName = namesArray [n];
+
+console.log(namesArray.length);
+
+console.log(randomName);
+
+let arrayAsString = namesArray.join(', ');
+console.log(arrayAsString);
+console.log(namesArray);
+
+$('#name').html(namesArray[n]);
+
+$('#names').html(arrayAsString);
